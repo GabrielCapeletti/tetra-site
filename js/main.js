c23badec57a423df2ead3fdd96bc04ff5a4643a2
Gabriel <gabriel@cafundo.tv>
2018-05-30 20:02:23 -0300

    ADD:Parallax on x

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
 $(function () {
+        var parallaxMouseStrenght = 50;
+
+        var offsetX = 0;        
+        var offsetY = 0;
+        var top = 0;
         resizeContentMargin();
     
-        window.addEventListener("scroll", function(event){            
-            var top = this.pageYOffset;
+        $(window).scroll(function(event){            
+            top = this.pageYOffset;           
+            applyParallax();
+        });
+        
+    
+        function resizeContentMargin(){
+            var height = $('[data-parallax]').height();    
+            $('[data-content]').css("margin-top",height*1.14);
+        }
 
-            var layers = document.getElementsByClassName("parallax");
+        function applyParallax(){
+            var layers = $('.parallax');
             var layer, speed, yPos;
             for (var i = 0; i < layers.length; i++) {
                 layer = layers[i];
-                speed = layer.getAttribute('data-speed');
-                var yPos = -(top * speed / 100);
-                layer.setAttribute('style', 'transform: translate3d(-50%, ' + yPos + 'px, 0px)');
-
+                speed = $(layer).attr('data-speed');
+                var yPos = -((top+offsetY) * speed / 100);
+                var xPos = -(offsetX * speed/100);
+                $(layer).css('transform', 'translate3d(calc(-50% - '+xPos+'px), ' + yPos + 'px, 0px)');
             }
-        });
-    
-        function resizeContentMargin(){
-            var height = $('#parallax-header').height();    
-            $('#content').css("margin-top",height*1.14);
+        }
+
+        function onMouseMove(event) {
+            var winWindth = $(window).width();
+            var winHeight = $(window).height();
+
+            offsetX = parallaxMouseStrenght* event.pageX/winWindth;
+            //offsetY = parallaxMouseStrenght* event.pageY/winHeight;
+
+            applyParallax();
         }
 
         $(window).resize(resizeContentMargin);
+        $(window).mousemove(onMouseMove);
 });
