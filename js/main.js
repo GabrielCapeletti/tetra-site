fbd8023eedb37521e753013c717504eeb93b386d
Gabriel <gabriel@cafundo.tv>
2018-05-31 18:54:37 -0300

    ADD: Complete site

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ $(function () {
             applyParallax();
         });
         
-    
+         $('[data-modal-img]').css("margin-top",$(window).height()/2);
+
         function resizeContentMargin(){
             var height = $('[data-parallax]').height();    
             $('[data-content]').css("margin-top",height*1.14);
@@ -24,7 +25,7 @@ $(function () {
                 layer = layers[i];
                 speed = $(layer).attr('data-speed');
                 var yPos = -((top+offsetY) * speed / 100);
-                var xPos = -(offsetX * speed/100);
+                var xPos = -(offsetX * speed/100);         
                 $(layer).css('transform', 'translate3d(calc(-50% - '+xPos+'px), ' + yPos + 'px, 0px)');
             }
         }
@@ -33,10 +34,30 @@ $(function () {
             var winWindth = $(window).width();
             var winHeight = $(window).height();
 
-            offsetX = parallaxMouseStrenght* event.pageX/winWindth;
+            offsetX = parallaxMouseStrenght * ((event.pageX/winWindth)-0.5);
             //offsetY = parallaxMouseStrenght* event.pageY/winHeight;
 
             applyParallax();
+        }
+
+        var modal = $('[data-modal]');        
+        var imgs = $('[data-img]');
+        var modalImg =  $('[data-modal-img]');        
+
+        for (var i = 0; i < imgs.length; i++) {
+            element = imgs[i];
+            element.onclick = function(){
+                $('[data-modal]').css("display","block");
+                modalImg.attr("src",this.src);                
+            }    
+        }
+       
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {             
+            $('[data-modal]').css("display","none");
         }
 
         $(window).resize(resizeContentMargin);
